715f9cacccec187405977931ee8a87b0814ef428
Paritosh <paritosh@Paritoshs-MacBook-Pro.local>
2020-12-20 14:46:14 +0530

    desktop fix | mobile design version

--- app.js
+++ app.js
@@ -6,19 +6,27 @@ for (let ques of quesClass) {
   ques.addEventListener("click", (eve) => {
     let ans = eve.target.parentNode.nextElementSibling;
     let arrowSibling = eve.target.nextElementSibling;
-    console.log(arrowSibling);
+
     if (toggle) {
-      ques.style.color = "hsl(237, 12%, 33%)";
-      ques.style.fontWeight = "600";
-      arrowSibling.style.transform = "rotate(180deg)";
-      ans.style.display = "block";
+      showAns(ques, arrowSibling, ans);
       toggle = false;
     } else {
-      ques.style.color = "hsl(240, 6%, 50%)";
-      ques.style.fontWeight = "400";
-      arrowSibling.style.transform = "rotate(360deg)";
-      ans.style.display = "none";
+      hideAns(ques, arrowSibling, ans);
       toggle = true;
     }
   });
+}
+
+function showAns(ques, arrowSibling, ans) {
+  ques.style.color = "hsl(237, 12%, 33%)";
+  ques.style.fontWeight = "600";
+  arrowSibling.style.transform = "rotate(180deg)";
+  ans.style.display = "block";
+}
+
+function hideAns(ques, arrowSibling, ans) {
+  ques.style.color = "hsl(240, 6%, 50%)";
+  ques.style.fontWeight = "400";
+  arrowSibling.style.transform = "rotate(360deg)";
+  ans.style.display = "none";
 }
